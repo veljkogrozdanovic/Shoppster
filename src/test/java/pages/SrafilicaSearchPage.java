2120235dd7faae670afb70eee4f33e51dd6c8a72
Veljko <Veljko@DESKTOP-6I20H84>
2022-11-25 22:22:11 +0100

    First task

---
+++ src/test/java/pages/SrafilicaSearchPage.java
@@ -0,0 +1,43 @@
+package pages;
+
+import helpers.BaseHelper;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class SrafilicaSearchPage extends BaseHelper
+{
+    @FindBy(xpath = "/html/body/ung-root/ung-storefront/main/cx-page-layout/cx-page-slot[2]/ung-product-facet-navigation/div/div[2]/ul/li[3]/div/label/input")
+    WebElement boschCheckbox;
+
+    @FindBy(xpath = "/html/body/ung-root/ung-storefront/main/cx-page-layout/cx-page-slot[2]/ung-product-facet-navigation/div/div[4]/ul/li[2]/div/label/input")
+    WebElement priceRange;
+
+
+
+    public SrafilicaSearchPage(WebDriver driver)
+    {
+        this.driver = driver;
+        PageFactory.initElements(driver,this);
+    }
+
+    private void SelectBosch()
+    {
+        wdWait.until(ExpectedConditions.visibilityOf(boschCheckbox));
+        boschCheckbox.click();
+    }
+    private void SelectPriceRange()
+    {
+        wdWait.until(ExpectedConditions.visibilityOf(priceRange));
+        priceRange.click();
+    }
+
+    public void ChooseSraflica() throws InterruptedException
+    {
+        SelectBosch();
+        Thread.sleep(2000);
+        SelectPriceRange();
+    }
+}
--- src/test/java/tests/ShoppsterTest.java
+++ src/test/java/tests/ShoppsterTest.java
@@ -1,14 +1,44 @@
 package tests;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import pages.ShoppsterHomePage;
+import pages.SrafilicaSearchPage;
+
+import java.util.List;
 
 public class ShoppsterTest extends BaseTest
 {
     @Test
-    public void shopsterTest() throws InterruptedException {
+    public void shopsterTest() throws InterruptedException
+    {
+        String searchTerm = "Srafilica";
+        String brand = "Bosch";
         ShoppsterHomePage shopsterHomePage = new ShoppsterHomePage(driver);
+        SrafilicaSearchPage srafilicaSearchPage = new SrafilicaSearchPage(driver);
 
-        shopsterHomePage.choosItems("Srafilica");
+        shopsterHomePage.choosItems(searchTerm);
+        Thread.sleep(1500);
+        srafilicaSearchPage.ChooseSraflica();
+        Thread.sleep(1500);
+
+        WebElement results = driver.findElement(By.className("plp__title"));
+        Assert.assertTrue("Search term doesn't match",results.getText().contains(searchTerm));
+
+        WebElement resultsContainer = driver.findElement(By.className("row"));
+        List<WebElement> articlesList = resultsContainer.findElements(By.className("plp__item"));
+        System.out.println("Number of articles is:"+articlesList.size());
+
+        for (WebElement article : articlesList)
+        {
+            WebElement elementTittle = article.findElement(By.tagName("a"));
+            Assert.assertTrue("Tittle doesn't contain requested Brand.\nTittle is: "+elementTittle.getText()+ "\nRequested brand: "+brand,
+                    elementTittle.getText().contains(brand));
+        }
+
+        Thread.sleep(1000);
     }
 }
